13272ab543e02476ebee0b9b0658f5853ce43b16
Jahir Valverde <jahirgabvalmur@gmail.com>
2024-03-29 18:45:34 -0600

    first commit

---
+++ ord_por_insercion/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  int size = 5;
+  int* arr_to_order = (int*) calloc(size, sizeof(int));
+  if (!arr_to_order) {
+    return EXIT_FAILURE;
+  }
+  *(arr_to_order + 0) = 5; *(arr_to_order + 1) = 2; *(arr_to_order + 2) = 7;
+  *(arr_to_order + 3) = 10; *(arr_to_order + 4) = 1;
+
+  /// [5, 2, 7, 10, 1]
+  /// 1. [1, 2, 7, 10, 5]
+  /// 2. [1, 2, 7, 10, 5]
+  /// 3. [1, 2, 5, 10, 7]
+  /// 4. [1, 2, 5, 7, 10]
+
+  int min = 0;
+  int temp = 0;
+  for (int i = 0; i < size; i++) {
+    min = i;
+    for (int j = i + 1; j < size; j++) {
+      if (arr_to_order[j] < arr_to_order[min]) {
+        min = j;
+      }
+    }
+    temp = arr_to_order[i];
+    arr_to_order[i] = arr_to_order[min];
+    arr_to_order[min] = temp;
+  }
+
+  for (int i = 0; i < size; i++) {
+    fprintf(stdout, "A[%" PRId64 "] = %" PRId64 "\n", i, arr_to_order[i]);
+  }  
+}
